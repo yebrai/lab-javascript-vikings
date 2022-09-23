35bf09cbe62157563db92c4c4de6c60020c99b62
Yebrai <ivansevv@gmail.com>
2022-09-23 21:29:30 +0200

    Half it4 bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,85 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthParam, strengthParam) {
+        this.health = healthParam;
+        this.strength = strengthParam
+    }
 
+    attack = () => {
+        return this.strength
+    }
+    receiveDamage = (damage) => {
+         this.health = this.health - damage
+    }
+}
+    
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(nameParam, healthParam, strengthParam) {
+        super(healthParam, strengthParam)
+        this.name = nameParam
+    }
+
+    receiveDamage = (damage) => {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else if (this.health <= damage) {
+            return `${this.name} has died in act of combat` 
+        }
+    }
+    
+ battleCry = () => {
+    return "Odin Owns You All!"
+}
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+    constructor(healthParam, strengthParam) {
+    super(healthParam, strengthParam)
+    }
+
+
+    receiveDamage = (damage) => {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else if (this.health <= damage) {
+            return "A Saxon has died in combat" 
+        }
+
+    }  
+    
+}
 
 // War
-class War {}
+class War {
+    constructor () {
+        this.vikingArmy = [];     
+        this.saxonArmy = []  
+    }
+    addViking =(vikingObj) => {
+        this.vikingArmy.push(vikingObj)
+    }
+    addSaxon =(saxonObj) => {
+        this.saxonArmy.push(saxonObj)
+    }
+    vikingAttack =() => {
+        randomSaxon = Math.random() / this.saxonArmy.length;
+        ramdomInt = Math.floor(random);
+        if (Saxon.receiveDamage() = Viking.strength) {
+            this.saxonArmy.pop()
+        }
+        
+    }
+    saxonAttack =() => {
+
+    }
+    showStatus =() => {
+
+    }
+
+}
